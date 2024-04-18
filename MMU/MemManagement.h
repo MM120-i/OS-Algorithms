d028c8188ed55f575624bcc988f335351ceba88a
Mahim <80307451+MM120-i@users.noreply.github.com>
2024-04-18 01:59:21 -0400

    Update MemManagement.h

--- MMU/MemManagement.h
+++ MMU/MemManagement.h
@@ -15,10 +15,17 @@ typedef struct
 
 // Function prototypes
 void init_memory(MemoryBlock *memory, int size);
-int allocate_memory(MemoryBlock *memory, int size);
+int firstFitAllocate(MemoryBlock *memory, int size);
 void deallocate_memory(MemoryBlock *memory, int address);
 void compact_memory(MemoryBlock *memory);
 void handle_data_structure(int choice, MemoryBlock *memory);
-void handle_linked_list(MemoryBlock *memory);
+void handleAlgorithm(MemoryBlock *memory);
+int bestFitAllocate(MemoryBlock *memory, int size);
+
+// Algorithms for memory mangement
+void firstFit(MemoryBlock *memory);
+void bestFit(MemoryBlock *memory);
+void worstFit(MemoryBlock *memory);
+void nextFit(MemoryBlock *memory);
 
 #endif
