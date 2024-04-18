ecfd09097204b823b0347e71af48aa99f4e1eed9
Mahim <80307451+MM120-i@users.noreply.github.com>
2024-04-18 01:59:38 -0400

    Update MemManagement.c

--- MMU/MemManagement.c
+++ MMU/MemManagement.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include "MemManagement.h"
 
@@ -16,7 +17,7 @@ void init_memory(MemoryBlock *memory, int size)
 /**
  * Function to allocate memory using First-Fit algorithm.
  */
-int allocate_memory(MemoryBlock *memory, int size)
+int firstFitAllocate(MemoryBlock *memory, int size)
 {
     int start_address = -1;
 
@@ -112,4 +113,69 @@ void compact_memory(MemoryBlock *memory)
     {
         memory[i].is_allocated = 0;
     }
+}
+
+/**
+ * Function to allocate memory using the Best-Fit algorithm.
+ */
+int bestFitAllocate(MemoryBlock *memory, int size)
+{
+    int start_address = -1;
+    int min_size = INT_MAX;
+
+    for (size_t i = 0; i < MEMORY_SIZE; i++)
+    {
+        // Check if the block is free and large enough to accommodate the request size.
+        if (!memory[i].is_allocated && memory[i].size >= size)
+        {
+            // Check if the current block size is smaller than the minimum size found so far.
+            if (memory[i].size < min_size)
+            {
+                start_address = memory[i].start_address;
+                min_size = memory[i].size;
+            }
+        }
+    }
+
+    // if start address is still -1, no suitable block was found
+    if (start_address == -1)
+    {
+        printf("No suitable block was found.\n");
+        return -1;
+    }
+
+    // Allocate memory and update block info.
+    for (size_t i = 0; i < MEMORY_SIZE; i++)
+    {
+        if (memory[i].start_address == start_address)
+        {
+            memory[i].is_allocated = 1;
+
+            // Split block if necessary
+            if (memory[i].size > size)
+            {
+                // Create a new block after the allocated block.
+                MemoryBlock newBlock;
+                newBlock.start_address = start_address + size;
+                newBlock.size = memory[i].size - size;
+                newBlock.is_allocated = 0;
+
+                // Shift memory blocks to make space for the new block.
+                for (size_t j = MEMORY_SIZE - 1; j > i; --j)
+                {
+                    memory[j] = memory[j - 1];
+                }
+
+                // Insert the new block after the allocated block.
+                memory[i + 1] = newBlock;
+
+                // Update the size of the aloocated block
+                memory[i].size = size;
+            }
+        }
+
+        break;
+    }
+
+    return start_address;
 }
