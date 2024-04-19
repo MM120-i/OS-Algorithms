3ae70985eaf1b9a23cbcd23d6887fcd836426954
Mahim <80307451+MM120-i@users.noreply.github.com>
2024-04-18 22:45:57 -0400

    Update MemManagement.h

--- MMU/MemManagement.h
+++ MMU/MemManagement.h
@@ -3,9 +3,10 @@
 
 #define MEMORY_SIZE 1024  // Total memory size in bytes
 #define SIZE sizeof(Node) // Size of each node in bytes
+#define ITERATIONS 1000000
 
 // Memory block structure
-typedef struct
+typedef struct MemoryBlock
 {
     int start_address;
     int size;
@@ -14,13 +15,15 @@ typedef struct
 } MemoryBlock;
 
 // Function prototypes
-void init_memory(MemoryBlock *memory, int size);
-int firstFitAllocate(MemoryBlock *memory, int size);
-void deallocate_memory(MemoryBlock *memory, int address);
+MemoryBlock *init_memory(int size);
+int firstFitAllocate(MemoryBlock *memory, int size, int memory_size);
+void deallocate_memory(MemoryBlock *memory, int address, int memory_size);
 void compact_memory(MemoryBlock *memory);
 void handle_data_structure(int choice, MemoryBlock *memory);
 void handleAlgorithm(MemoryBlock *memory);
-int bestFitAllocate(MemoryBlock *memory, int size);
+int bestFitAllocate(MemoryBlock *memory, int size, int memory_size);
+int worstFitAllocate(MemoryBlock *memory, int size, int memory_size);
+double timeDiff(clock_t start, clock_t end);
 
 // Algorithms for memory mangement
 void firstFit(MemoryBlock *memory);
